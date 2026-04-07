c04a190067147caf071fadc6b9ce2ebcca081c31
yashwantray361 <yashwantray361@gmail.com>
2026-04-08 02:14:17 +0530

    implement user login functionality and authentication middleware

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -80,3 +80,43 @@ export const verifyUser = tryCatch(async (req, res) => {
 
 })
 
+export const loginUser = tryCatch(async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(400).json({ message: 'Wrong Email' })
+    }
+
+    const matchPassword = await bcrypt.compare(password, user.password)
+    if (!matchPassword) {
+        return res.status(400).json({
+            message: 'Wrong Password'
+        });
+    }
+
+    const token = jwt.sign(
+        { _id: user._id },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: '15d'
+        }
+    );
+
+    return res.status(200).json({
+        message: `Welcome back ${user.name}`,
+        token,
+        user
+    });
+
+})
+
+export const myProfile = tryCatch(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    res.json({ user })
+})
+
+
+
---
+++ server/middleware/isAuth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken'
+import { User } from '../models/user.js'
+
+export const isAuth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({
+                message: 'Please login first'
+            });
+        }
+
+        const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decodedData._id);
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found'
+            });
+        }
+
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Invalid or expired token'
+        });
+    }
+};
